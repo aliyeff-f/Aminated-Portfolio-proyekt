ad1f41a8d035a749a488bc2a8d0f20382b356997
aliyeff-f <eliyevnurlan811@gmail.com>
2024-03-25 14:45:15 +0400

    header

---
+++ Photographer portfolio/assets/js/script.js
@@ -0,0 +1,65 @@
+'use strict';
+
+
+
+// add Event on multiple elment
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+
+
+// PRELOADING
+
+const loadingElement = document.querySelector("[data-loading]");
+
+window.addEventListener("load", function () {
+  loadingElement.classList.add("loaded");
+  document.body.classList.remove("active");
+});
+
+
+
+// MOBILE NAV TOGGLE
+
+const [navTogglers, navLinks, navbar, overlay] = [
+  document.querySelectorAll("[data-nav-toggler]"),
+  document.querySelectorAll("[data-nav-link]"),
+  document.querySelector("[data-navbar]"),
+  document.querySelector("[data-overlay]")
+];
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+const closeNav = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+  document.body.classList.remove("active");
+}
+
+addEventOnElements(navLinks, "click", closeNav);
+
+
+
+// HEADER
+
+const header = document.querySelector("[data-header]");
+
+const activeElementOnScroll = function () {
+  if (window.scrollY > 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+window.addEventListener("scroll", activeElementOnScroll);
