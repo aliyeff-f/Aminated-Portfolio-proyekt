90ce5e9d4308c7632a233828720354af4fa92983
aliyeff-f <eliyevnurlan811@gmail.com>
2024-03-27 13:33:04 +0400

    finished

--- Photographer portfolio/assets/js/script.js
+++ Photographer portfolio/assets/js/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-
-
-// add Event on multiple elment
-
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener(eventType, callback);
@@ -11,8 +7,6 @@ const addEventOnElements = function (elements, eventType, callback) {
 }
 
 
-
-// PRELOADING
 
 const loadingElement = document.querySelector("[data-loading]");
 
@@ -22,8 +16,6 @@ window.addEventListener("load", function () {
 });
 
 
-
-// MOBILE NAV TOGGLE
 
 const [navTogglers, navLinks, navbar, overlay] = [
   document.querySelectorAll("[data-nav-toggler]"),
@@ -49,9 +41,6 @@ const closeNav = function () {
 addEventOnElements(navLinks, "click", closeNav);
 
 
-
-// HEADER
-
 const header = document.querySelector("[data-header]");
 
 const activeElementOnScroll = function () {
@@ -72,55 +61,54 @@ let totalLetterBoxDelay = 0;
 
 const setLetterEffect = function () {
 
-  // loop through all letter boxes
+  
   for (let i = 0; i < letterBoxes.length; i++) {
-    // set initial animation delay
+    
     let letterAnimationDelay = 0;
 
-    // get all character from the current letter box
+    
     const letters = letterBoxes[i].textContent.trim();
-    // remove all character from the current letter box
+    
     letterBoxes[i].textContent = "";
 
-    // loop through all letters
+    
     for (let j = 0; j < letters.length; j++) {
 
-      // create a span
+     
       const span = document.createElement("span");
 
-      // set animation delay on span
+      
       span.style.animationDelay = `${letterAnimationDelay}s`;
 
-      // set the "in" class on the span, if current letter box is active
-      // otherwise class is "out"
+      
       if (i === activeLetterBoxIndex) {
         span.classList.add("in");
       } else {
         span.classList.add("out");
       }
 
-      // pass current letter into span
+      
       span.textContent = letters[j];
 
-      // add space class on span, when current letter contain space
+      
       if (letters[j] === " ") span.classList.add("space");
 
-      // pass the span on current letter box
+      
       letterBoxes[i].appendChild(span);
 
-      // skip letterAnimationDelay when loop is in the last index
+      
       if (j >= letters.length - 1) break;
-      // otherwise update
+      
       letterAnimationDelay += 0.05;
 
     }
 
-    // get total delay of active letter box
+    
     if (i === activeLetterBoxIndex) {
       totalLetterBoxDelay = Number(letterAnimationDelay.toFixed(2));
     }
 
-    // add active class on last active letter box
+    
     if (i === lastActiveLetterBoxIndex) {
       letterBoxes[i].classList.add("active");
     } else {
@@ -132,7 +120,7 @@ const setLetterEffect = function () {
   setTimeout(function () {
     lastActiveLetterBoxIndex = activeLetterBoxIndex;
 
-    // update activeLetterBoxIndex based on total letter boxes
+    
     activeLetterBoxIndex >= letterBoxes.length - 1 ? activeLetterBoxIndex = 0 : activeLetterBoxIndex++;
 
     setLetterEffect();
@@ -140,5 +128,81 @@ const setLetterEffect = function () {
 
 }
 
-// call the letter effect function after window loaded
+
 window.addEventListener("load", setLetterEffect);
+
+
+
+
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  const bodyHeight = document.body.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const scrollEndPos = bodyHeight - windowHeight;
+  const totalScrollPercent = (window.scrollY / scrollEndPos) * 100;
+
+  backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
+
+  
+  if (totalScrollPercent > 5) {
+    backTopBtn.classList.add("show");
+  } else {
+    backTopBtn.classList.remove("show");
+  }
+});
+
+
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < revealElements.length; i++) {
+    const elementIsInScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15;
+
+    if (elementIsInScreen) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+}
+
+window.addEventListener("scroll", scrollReveal);
+
+scrollReveal();
+
+
+const cursor = document.querySelector("[data-cursor]");
+const anchorElements = document.querySelectorAll("a");
+const buttons = document.querySelectorAll("button");
+
+
+document.body.addEventListener("mousemove", function (event) {
+  setTimeout(function () {
+    cursor.style.top = `${event.clientY}px`;
+    cursor.style.left = `${event.clientX}px`;
+  }, 100);
+});
+
+
+const hoverActive = function () { cursor.classList.add("hovered"); }
+
+
+const hoverDeactive = function () { cursor.classList.remove("hovered"); }
+
+
+addEventOnElements(anchorElements, "mouseover", hoverActive);
+addEventOnElements(anchorElements, "mouseout", hoverDeactive);
+addEventOnElements(buttons, "mouseover", hoverActive);
+addEventOnElements(buttons, "mouseout", hoverDeactive);
+
+
+document.body.addEventListener("mouseout", function () {
+  cursor.classList.add("disabled");
+});
+
+
+document.body.addEventListener("mouseover", function () {
+  cursor.classList.remove("disabled");
+});
